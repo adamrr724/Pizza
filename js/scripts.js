e3aa133b151de9619e58282399e35e60ce1bc410
adam ross russell and jon sweeney <student@epicodus.com>
2016-01-28 15:43:06 -0800

    added toppings and size javascript logic

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,25 @@
+function Pizza(size, toppings) {
+  this.size = size;
+  this.toppings = toppings;
+  this.minPizzaCost = 8;
+  this.sizePizzaCost = this.sizePricingMethod();
+}
 
+Pizza.prototype.sizePricingMethod = function () {
+
+  if (this.size === "Small") {
+    return this.minPizzaCost;
+  } else if (this.size === "Medium") {
+    return this.minPizzaCost + 2;
+  }
+ return this.minPizzaCost + 4;
+};
+
+Pizza.prototype.toppingsPricingMethod = function () {
+  if (this.toppings.length  === 1){
+    return this.sizePizzaCost + 1;
+  } else if (this.toppings.length === 2){
+    return this.sizePizzaCost + 2;
+  }
+  return this.sizePizzaCost + 3;
+};
