7d63c59c8e46854446646fb9fd29feb3d9f59d7c
adam ross russell and jon sweeney <student@epicodus.com>
2016-01-28 16:29:15 -0800

    add new logic for toppings price

--- js/scripts.js
+++ js/scripts.js
@@ -10,16 +10,17 @@ Pizza.prototype.sizePricingMethod = function () {
   if (this.size === "Small") {
     return this.minPizzaCost;
   } else if (this.size === "Medium") {
-    return this.minPizzaCost + 2;
+    return this.minPizzaCost + 3;
   }
- return this.minPizzaCost + 4;
+ return this.minPizzaCost + 6;
 };
 
 Pizza.prototype.toppingsPricingMethod = function () {
-  if (this.toppings.length  === 1){
-    return this.sizePizzaCost + 1;
-  } else if (this.toppings.length === 2){
-    return this.sizePizzaCost + 2;
-  }
-  return this.sizePizzaCost + 3;
-};
+
+for (var i = 0; i < this.toppings.length; i++) {
+  this.sizePizzaCost += 1;
+}
+return this.sizePizzaCost;
+}
+
+var newPizza = new Pizza ("Medium", ["Pepperoni", "Green Peppers"]);
